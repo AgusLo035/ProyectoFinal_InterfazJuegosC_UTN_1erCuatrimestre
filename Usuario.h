7cf626ade238e6fe196ec3badcc0772553c4b6dd
sebapoey <sdpoey@gmail.com>
2026-06-15 20:17:45 -0300

    notas (de hacer) agregadas a usuario
    el "AMBCL" este aplica a la estructura de usuarios también
    
    hoy/mañana te ayudo, algunas probablemente sean copy paste de las funciones en juegos.h
    sobre ideas, creo sería incorporar el consulta/listado a una cuenta admin

--- Usuario.h
+++ Usuario.h
@@ -15,6 +15,13 @@ typedef struct
 
 } Usuario;
 
+
+///No te quiero tocar esto por si acaso, lo podes ordenar en Alta, Baja, Modificación, Consulta, Listados?
+///Igual veo faltan los últimos 3 de modif. consul. y listados que hay que pensar cómo hacerlos
+///Tal vez cada usuario puede modificar su nombre y el admin puede acceder a consulta y listados (en alguno de estos dos va la idea de la pila de la profe de los usuarios más activos también) y listados
+///(^ esto porque el "ABMCL" la consigna dice hacerlo en relación a CADA UNA de las DOS structs que se piden como mínimo)
+///(hoy/mañana te ayudo pq algunos creo sería literal copiar y pegar (con mini modificaciones) las funciones que ya tenemos en juegos)
+
 // Borrado Logico
 void eliminarUsuario(Usuario *usuarioAEliminar);
 
